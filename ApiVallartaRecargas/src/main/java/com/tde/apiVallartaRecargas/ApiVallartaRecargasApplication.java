7436432271afd731ac8e0b923879378e73dccc35
Emmanuelhhh <eme.leiuv@gmail.com>
2025-11-20 12:02:26 -0600

    Primera verision base para iniciar proyecto

---
+++ ApiVallartaRecargas/src/main/java/com/tde/apiVallartaRecargas/ApiVallartaRecargasApplication.java
@@ -0,0 +1,13 @@
+package com.tde.apiVallartaRecargas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVallartaRecargasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVallartaRecargasApplication.class, args);
+	}
+
+}
---
+++ ApiVallartaRecargas/src/test/java/com/tde/apiVallartaRecargas/ApiVallartaRecargasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tde.apiVallartaRecargas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVallartaRecargasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
